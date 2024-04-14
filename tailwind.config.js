5976ec453d785e896b5cb08f11abc29cd31e56af
Leandro Basilio <lesafe07@gmail.com>
2024-04-13 21:25:31 -0300

    Site feito até aqui

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'playfair': ['Playfair Display', 'serif']
+      },
+      translate: {
+        '35': '154px',
+      },
+    }
+  },
+  plugins: [],
+}
+
